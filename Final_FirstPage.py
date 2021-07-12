08c11e9d356c13a1979028fd7c40b2e52af4b046
Aritra Marik <65804948+devilishAM@users.noreply.github.com>
2021-07-12 22:46:36 +0530

    First Page

--- Final_FirstPage.py
+++ Final_FirstPage.py
@@ -20,6 +20,7 @@ root.configure(bg='midnight blue')
 root.geometry("500x450+630+70")
 root.maxsize(500,450)
 root.minsize(500,450)
+
 firebase=pyrebase.initialize_app(firebaseConfig)
 db= firebase.database()
 
@@ -30,9 +31,13 @@ def signin(username,password):
     name = username
     password = password
     auth.sign_in_with_email_and_password(name, password)
+    print("Done")
 
 def account():
     import Accounts
+
+def Loan_HomePage():
+    import Loan_HomePage
     
 def page1():
     def LoginPage():
@@ -77,8 +82,49 @@ def page1():
             b10 = Button(root, bg='gold', fg='black', text="DATA ENTRY", borderwidth=5, font="Helvetica 10 bold",command=account)
             b10.place(x=170, y=200)
             b20 = Button(root, bg='gold', fg='black', text="LOAN", borderwidth=5, font="Helvetica 10 bold",
-                         command=None)
+                         command=Loan_HomePage)
             b20.place(x=295, y=200)
+
+            # def Time():
+            #     string = strftime('%H:%M:%S %p')
+            #     Time_Label.config(text=string)
+            #     Time_Label.after(1000, Time)
+
+
+            # Time_Label = Label(root, fg="black", bg="gold",
+            #         font="Devanagari 15 bold", borderwidth=4, relief=SUNKEN)
+            # Time_Label.place(x=426, y=2)
+            # Time()
+
+            # def srch():
+            #     balance=Label(root,fg='gold',bg='midnight blue',font="Devanagari 15 bold",text="Balance:-")
+            #     balance.place(x=100,y=175)
+            #     Loan=Label(root,fg='gold',bg='midnight blue',font="Devanagari 15 bold",text="Loan:-")
+            #     Loan.place(x=320,y=175)
+
+            # date = Label(root, text=f"{dt.datetime.now():%a, %b/%d/%Y}", bg="gold", fg="black", font=(
+            # "helvetica 15 bold"), borderwidth=4, relief=SUNKEN)
+            # date.place(x=1, y=2)
+
+            # name=StringVar()
+            # name.set("Enter Customer Name Here")
+            # customer_name=Entry(root,justify=CENTER,borderwidth=4,textvariable=name,font="Helvetica 12 bold",width=30)
+            # customer_name.place(x=140,y=80)
+
+            # Check=Button(root,text="Search",bg='gold',font="Helvetica 11 bold",borderwidth=4,relief=RAISED,command=srch)
+            # Check.place(x=353,y=125)
+
+            # accounts=Button(root,text="Accounts",bg='gold',font="Helvetica 11 bold",borderwidth=4,relief=SUNKEN)
+            # accounts.place(x=180,y=240)
+            # loan=Button(root,text="Loan",bg='gold',font="Helvetica 11 bold",borderwidth=4,relief=SUNKEN)
+            # loan.place(x=320,y=240)
+
+            # Treasury=Label(root,bg='gold',fg='black',font="Helvetica 13 bold",text="Treasury:-")
+            # Treasury.place(x=70,y=350)
+
+            # root.mainloop()
+
+
         except:
             tkinter.messagebox.showerror('Error', 'User not registered. Please Register at first')
             page1()
