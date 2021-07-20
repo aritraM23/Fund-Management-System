9c66162de6430c827ab7e48672556733ebd47f97
Aritra Marik <65804948+devilishAM@users.noreply.github.com>
2021-07-20 11:40:41 +0530

    Create HomePage.py

--- HomePage.py
+++ HomePage.py
@@ -1,6 +1,29 @@
-from tkinter import*
+import csv
+from functools import partial
+import pandas as pd
+from tkinter import *
+from tkinter import ttk
+import tkinter.messagebox
+import os
 from time import strftime
+from datetime import datetime
+import time
+import pyrebase
+import mysql.connector
 import datetime as dt
+
+firebaseConfig = {'apiKey': "AIzaSyDR-a5PGjXpXFjvJVS9Ep3FOKXnNy9BsZg",
+    'authDomain': "fundmang-42ad8.firebaseapp.com",
+    'projectId': "fundmang-42ad8",
+    'storageBucket': "fundmang-42ad8.appspot.com",
+    'messagingSenderId': "361815074904",
+    'databaseURL':'https://fundmang-42ad8-default-rtdb.firebaseio.com',
+    'appId': "1:361815074904:web:8504dfd52dbde0b186422d",
+    'measurementId': "G-MVMXL8CJNK"
+}
+
+firebase=pyrebase.initialize_app(firebaseConfig)
+db= firebase.database()
 
 root = Tk()
 root.geometry("550x450+600+90")
@@ -8,6 +31,28 @@ root.config(bg="midnight blue")
 root.title("HomePage")
 root.maxsize(550,450)
 root.minsize(550,450)
+
+name=StringVar()
+amount = 0
+
+def ind_Bal():
+    balance = 0
+    indData = db.child('registerUserExp').child(name.get()).get()
+    for ind in indData.each():
+        balance += int(ind.val()['amount'])
+
+    print(name.get() + str(balance))
+    return balance
+
+
+def treasure():
+    global amount
+    totalDB = db.child('mainData').get()
+    for data in totalDB.each():
+        amount += int(data.val()['amount'])
+
+    # print(amount)
+    return amount
 
 
 def LoanWindow():
@@ -28,7 +73,7 @@ Time()
 
 
 def search():
-    balance=Label(root,fg='gold',bg='midnight blue',font="Devanagari 15 bold",text="Balance:-")
+    balance=Label(root,fg='gold',bg='midnight blue',width= 30,font="Devanagari 15 bold",text="Balance:- " + str(ind_Bal()))
     balance.place(x=100,y=175)
     Loan=Label(root,fg='gold',bg='midnight blue',font="Devanagari 15 bold",text="Loan:-")
     Loan.place(x=320,y=175)
