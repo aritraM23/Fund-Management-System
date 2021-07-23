7f339e0d4544f6ebace93d604711cd37f3033b20
Aritra Marik <65804948+devilishAM@users.noreply.github.com>
2021-07-23 11:10:38 +0530

    Update Loan_RepayPage.py

--- Loan_RepayPage.py
+++ Loan_RepayPage.py
@@ -1,4 +1,21 @@
+import tkinter.messagebox
 from tkinter import *
+import pyrebase
+import mysql.connector
+
+
+firebaseConfig = {'apiKey': "AIzaSyDR-a5PGjXpXFjvJVS9Ep3FOKXnNy9BsZg",
+    'authDomain': "fundmang-42ad8.firebaseapp.com",
+    'projectId': "fundmang-42ad8",
+    'storageBucket': "fundmang-42ad8.appspot.com",
+    'messagingSenderId': "361815074904",
+    'databaseURL':'https://fundmang-42ad8-default-rtdb.firebaseio.com',
+    'appId': "1:361815074904:web:8504dfd52dbde0b186422d",
+    'measurementId': "G-MVMXL8CJNK"
+}
+
+firebase=pyrebase.initialize_app(firebaseConfig)
+db= firebase.database()
 
 root = Tk()
 root.geometry("550x450+600+90")
@@ -11,6 +28,24 @@ name_entry = StringVar()
 mob_entry = StringVar()
 p_entry = StringVar()
 i_entry = StringVar()
+
+def repay():
+    name = name_entry.get()
+    mobileNumber = mob_entry.get()
+    principlePaid = p_entry.get()
+    interestPaid = i_entry.get()
+    
+    loanInfo = db.child('loanData').get()
+    for info in loanInfo.each():
+        if (name == info.val()['name'] or mobileNumber == info.val()['mobileNumber']):
+            previousPrinciple = info.val()['principalLeft']
+            interstLeft = info.val()['interestLeft']
+            newPriciple = int(previousPrinciple) - int(principlePaid)
+            newInterst = int(interstLeft) - int(interestPaid)
+            db.child('loanData').child(info.key()).update({'principalLeft': newPriciple,
+                                                           'interestLeft': newInterst})
+    tkinter.messagebox.showinfo('Success','Priciple and Interest Added')
+
 
 f1 = Frame(root, bg='gold', borderwidth=10,
            relief=RAISED, width=500, height=55)
@@ -46,7 +81,7 @@ interest = Entry(root, width=27, textvariable=i_entry,
 interest.place(x=230, y=251)
 
 submit_button = Button(root, text="Add", bg='gold', fg='black',
-                       font="Helvetica 11 bold", relief=SUNKEN, command=None)
+                       font="Helvetica 11 bold", relief=SUNKEN, command=repay)
 submit_button.place(x=425, y=310)
 
 
