9c9e6d3ece15c7cd10f6b9e5e8fccb6f6b92ee66
Aritra Marik <65804948+devilishAM@users.noreply.github.com>
2021-07-25 00:45:10 +0530

    Update NewLoan.py

--- NewLoan.py
+++ NewLoan.py
@@ -10,9 +10,6 @@ root.geometry("580x470+500+130")
 root.maxsize(580, 470)
 root.minsize(580, 470)
 root.title("NewLoan Page")
-p1 = PhotoImage(file='[DIGICURE MAIN LOGO].png')
-root.iconphoto(FALSE,p1)
-
 
 Top_Frame = Frame(root, borderwidth=6, bg='gold', relief=RAISED)
 Top_Frame.pack(fill=X)
@@ -51,51 +48,6 @@ interest.place(x=135, y=320)
 
 
 # ------------------------FUNCTIONS-----------------------------#
-def back():
-    root.destroy()
-    import Homepage
-    return 0
-    
-
-amount = 0
-def treasureCheck(pAmount):
-    global amount
-    interest = 0
-    princi = 0
-    totalDB = db.child('mainData').get()
-    loanDb = db.child('loanData').get()
-    for data in totalDB.each():
-        amount += int(data.val()['amount'])
-
-    print(amount)
-    try:
-        for ld in loanDb.each():
-            interest += int(ld.val()['interestPaidTillDate'])
-        amount += interest
-        print(amount)
-        for ld in loanDb.each():
-            princi += int(ld.val()['principalLeft'])
-
-        amount -= princi 
-        amount -= int(pAmount)
-        print(amount)
-        if(amount<=0):
-            tkinter.messagebox.showerror('Balance Exhausted!!', "No Balance Left. Loan Can't be Provided!")
-            back()
-            return 0
-        
-    except:
-        amount -= int(pAmount)
-        print(amount)
-        if(amount<=0):
-            tkinter.messagebox.showerror('Balance Exhausted!!', "No Balance Left. Loan Can't be Provided!")
-            back()
-            return 0
-
-    print(amount)
-    # print(amount)
-    return amount
-
 
 def add_data():
     name = name_entry.get()
@@ -110,24 +62,22 @@ def add_data():
     principalLeft = principalAmount
     interestLeft = (int(principalAmount) * int(interestPercent)) / 100
     interestPaidTillDate = 0
+    updatedLoanDate = date
 
-    var = treasureCheck(principal_entry.get())
-    if(var==0):
-        return 0
     try:
         if (name != '' and mobileNumber != '' and principalAmount != '' and interestPercent != ''):
             datas = {'name': name, 'mobileNumber': mobileNumber, 'address': address, 'shopName': shopName, 'date': date,
                      'principalAmount': principalAmount,
                      'interestPercent': interestPercent, 'priciplePaid': principlePaid, 'interestPaid': interestPaid,
-                     'principalLeft': principalLeft, 'interestLeft': interestLeft,  'interestPaidTillDate' : interestPaidTillDate}
+                     'principalLeft': principalLeft, 'interestLeft': interestLeft,  'interestPaidTillDate' : interestPaidTillDate, 'lastPaidDate':updatedLoanDate}
             db.child('loanData').push(datas)
 
-            myDataBase = mysql.connector.connect(host="localhost", user="root", passwd="mancunian@2002",
+            myDataBase = mysql.connector.connect(host="localhost", user="root", passwd="12345",
                                                  database='ivsLoan')
             mycursor = myDataBase.cursor()
-            dataCollection = 'Insert into loanentry (name ,mobileNumber ,address ,shopName ,date ,pricipalAmount ,interestPercent ,principlePaid ,interestPaid, principleLeft , interestLeft, interestPaidTillDate) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+            dataCollection = 'Insert into loanentry (name ,mobileNumber ,address ,shopName ,date ,pricipalAmount ,interestPercent ,principlePaid ,interestPaid, principleLeft , interestLeft, interestPaidTillDate,dateGiven) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
             datas = [(name, mobileNumber, address, shopName, date, principalAmount, interestPercent, principlePaid,
-                      interestPaid, principalLeft, interestLeft, interestPaidTillDate)]
+                      interestPaid, principalLeft, interestLeft, interestPaidTillDate,updatedLoanDate)]
 
             mycursor.executemany(dataCollection, datas)
             myDataBase.commit()
@@ -136,12 +86,12 @@ def add_data():
     except:
         tkinter.messagebox.showinfo('Offline',
                                     'You are offline your data will be saved offline. Later please Sync Up to sync the databases')
-        myDataBase = mysql.connector.connect(host="localhost", user="root", passwd="mancunian@2002",
+        myDataBase = mysql.connector.connect(host="localhost", user="root", passwd="12345",
                                              database='ivsLoan')
         mycursor = myDataBase.cursor()
-        dataCollection = 'Insert into loanEntry (name ,mobileNumber ,address ,shopName ,date ,pricipalAmount ,interestPercent ,principlePaid ,interestPaid, principleLeft , interestLeft, interestPaidTillDate) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+        dataCollection = 'Insert into loanEntry (name ,mobileNumber ,address ,shopName ,date ,pricipalAmount ,interestPercent ,principlePaid ,interestPaid, principleLeft , interestLeft, InterestPaidTillDate, dateGiven) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
         datas = [(name, mobileNumber, address, shopName, date, principalAmount, interestPercent, principlePaid,
-                  interestPaid, interestPaidTillDate)]
+                  interestPaid, interestPaidTillDate,updatedLoanDate)]
 
         mycursor.executemany(dataCollection, datas)
         myDataBase.commit()
@@ -167,8 +117,6 @@ principal_entry.place(x=215, y=280)
 interest_entry.place(x=215, y=320)
 # -------------------------------------------------------------------------#
 # --------------------------------Button-----------------------------------#
-Back = Button(root, text="Back", bg='gold', font="Helvetica 11 bold", borderwidth=4, relief=RAISED, command=back)
-Back.place(x=20, y=50)
 add_btn = Button(root, text="Add", font="Arial 12 bold", bg="gold", fg="black", command=add_data)
 add_btn.place(x=270, y=370)
 root.mainloop()
