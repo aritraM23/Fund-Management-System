4222992b2836581a67be633695b628ac3a2eb488
ARNAB RAY <59610398+Arnab11917676@users.noreply.github.com>
2021-08-06 20:05:59 +0530

    Updated Homepage with env

--- Homepage.py
+++ Homepage.py
@@ -12,18 +12,10 @@ import pyrebase
 import mysql.connector
 import datetime as dt
 from PIL.ImageTk import PhotoImage
+from envVar import firebaseConfig as fc
 
-firebaseConfig = {'apiKey': "AIzaSyDR-a5PGjXpXFjvJVS9Ep3FOKXnNy9BsZg",
-    'authDomain': "fundmang-42ad8.firebaseapp.com",
-    'projectId': "fundmang-42ad8",
-    'storageBucket': "fundmang-42ad8.appspot.com",
-    'messagingSenderId': "361815074904",
-    'databaseURL':'https://fundmang-42ad8-default-rtdb.firebaseio.com',
-    'appId': "1:361815074904:web:8504dfd52dbde0b186422d",
-    'measurementId': "G-MVMXL8CJNK"
-}
 
-firebase=pyrebase.initialize_app(firebaseConfig)
+firebase=pyrebase.initialize_app(fc)
 db= firebase.database()
 
 root = Tk()
