e8fd7659fadb4126da1697263df7c7f2ab53a1cd
ARNAB RAY <59610398+Arnab11917676@users.noreply.github.com>
2021-08-06 20:04:19 +0530

    environMent update

---
+++ envVar.py
@@ -0,0 +1,10 @@
+firebaseConfig = {
+    'apiKey': "AIzaSyDTMWv-92WG5xZ1NvcVb7WlE0mjPAiosY4",
+    'authDomain': "ivslive.firebaseapp.com",
+    'projectId': "ivslive",
+    'storageBucket': "ivslive.appspot.com",
+    'messagingSenderId': "846488367861",
+    'appId': "1:846488367861:web:581efb46cf0210326a4bbc",
+	'databaseURL' : "https://ivslive-default-rtdb.firebaseio.com/",
+    'measurementId': "G-RBWZWSHQSQ"
+  }
