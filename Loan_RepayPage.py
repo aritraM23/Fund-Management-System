790ecf40086e44d0c9b84f22a51e9857acb36e38
Aritra Marik <65804948+devilishAM@users.noreply.github.com>
2021-08-12 17:28:58 +0530

    autofill updated

--- Loan_RepayPage.py
+++ Loan_RepayPage.py
@@ -60,7 +60,7 @@ def repay():
 															   'interestLeft': newInterst,
 															   'interestPaidTillDate': interestPaidTillDates,
 															   'lastPaidDate': updateDate})
-		myDataBase = mysql.connector.connect(host="localhost", user="root", passwd="12345",
+		myDataBase = mysql.connector.connect(host="localhost", user="root", passwd="mancunian@2002",
 											 database='ivsLoan')
 		mycursor = myDataBase.cursor()
 		mycursor.execute(
@@ -70,7 +70,7 @@ def repay():
 	except:
 		tkinter.messagebox.showinfo('No Internet',
 									'You are offline. Saving your data offline. Please sync your databases later')
-		myDataBase = mysql.connector.connect(host="localhost", user="root", passwd="12345",
+		myDataBase = mysql.connector.connect(host="localhost", user="root", passwd="mancunian@2002",
 											 database='ivsLoan')
 		mycursor = myDataBase.cursor()
 		
@@ -78,6 +78,39 @@ def repay():
 				'UPDATE loanEntry set principleLeft= %s , interestLeft =%s , InterestPaidTillDate =%s, dateGiven = %s where name=%s and mobileNumber = %s ',
 				(newPriciple, newInterst, interestPaidTillDates, updateDate, name, mobileNumber))
 		tkinter.messagebox.showinfo('Success', 'Priciple and Interest Added In Local DataBase. Please sync later')
+
+def updateText(data):
+    #update the drop down list
+    # Clear the listbox
+    my_list.delete(0, END)
+
+    # Add toppings to listbox
+    for item in data:
+        my_list.insert(END, item)
+
+def fillout(event):
+    # Delete whatever is in the entry box
+    name.delete(0, END)
+
+    # Add clicked list item to entry box
+    name.insert(0, my_list.get(ANCHOR))
+
+def check(event):
+    # print("hello")
+    # grab what was typed
+    typed = name_entry.get()
+    print(typed)
+
+    if typed == '':
+        data = listVal
+    else:
+        data = []
+        for item in listVal:
+            if typed.lower() in item.lower():
+                data.append(item)
+
+    # update our listbox with selected items
+    updateText(data)
 
 
 Back = Button(root, text="Back", bg='gold', font="Helvetica 11 bold", borderwidth=4, relief=RAISED, command=back)
@@ -94,37 +127,63 @@ name_label = Label(root, bg='midnight blue', fg='gold',
 name_label.place(x=165, y=135)
 mob_label = Label(root, bg='midnight blue', fg='gold',
 				  font="Helvetica 11 bold", text="Mobile No.")
-mob_label.place(x=129, y=175)
+mob_label.place(x=129, y=219)
 principal_label = Label(root, bg='midnight blue', fg='gold',
 						font="Helvetica 11 bold", text="Principal Given")
-principal_label.place(x=94, y=215)
+principal_label.place(x=94, y=249)
 interest_label = Label(root, bg='midnight blue', fg='gold',
 					   font="Helvetica 11 bold", text="Interest Given")
-interest_label.place(x=102, y=255)
+interest_label.place(x=102, y=279)
 
 date_label = Label(root, bg='midnight blue', fg='gold',
 				   font="Helvetica 11 bold", text="Date")
-date_label.place(x=172, y=295)
+date_label.place(x=172, y=309)
 
 name = Entry(root, width=27, textvariable=name_entry,
 			 borderwidth=8, relief=SUNKEN, font="Helvetica 11 bold")
 name.place(x=230, y=131)
+
+my_list = Listbox(root, font =('arial', 13, 'bold'),height = 2, width=25, justify='left')
+my_list.place(x = 232, y=170)
+
+listVal = []
+def getNameList():
+    listVal = []
+    listname = db.child('loanData').get()
+    for each in listname.each():
+        print(each.val()['name'])
+        listVal.append(each.val()['name'])
+
+    listVal = list(set(listVal))
+    return listVal
+
+listVal = getNameList()
+
+updateText(listVal)
+
+# Create a binding on the listbox onclick
+my_list.bind("<<ListboxSelect>>", fillout)
+
+# Create a binding on the entry box
+name.bind("<KeyRelease>", check)
+
+
 mobile = Entry(root, width=27, textvariable=mob_entry,
 			   borderwidth=8, relief=SUNKEN, font="Helvetica 11 bold")
-mobile.place(x=230, y=171)
+mobile.place(x=230, y=218)
 principal = Entry(root, width=27, textvariable=p_entry,
 				  borderwidth=8, relief=SUNKEN, font="Helvetica 11 bold")
-principal.place(x=230, y=211)
+principal.place(x=230, y=248)
 interest = Entry(root, width=27, textvariable=i_entry,
 				 borderwidth=8, relief=SUNKEN, font="Helvetica 11 bold")
-interest.place(x=230, y=251)
+interest.place(x=230, y=278)
 
 dateEnt = Entry(root, width=27, textvariable=date_L,
 				borderwidth=8, relief=SUNKEN, font="Helvetica 11 bold")
-dateEnt.place(x=230, y=291)
+dateEnt.place(x=230, y=308)
 
 submit_button = Button(root, text="Add", bg='gold', fg='black',
 					   font="Helvetica 11 bold", relief=SUNKEN, command=repay)
-submit_button.place(x=425, y=330)
+submit_button.place(x=425, y=350)
 
 root.mainloop()
