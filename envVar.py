74d31f710ccaa41a24b6d7f310cd9feaa88a9201
Aritra Marik <65804948+devilishAM@users.noreply.github.com>
2021-09-04 23:32:16 +0530

    final commit

--- envVar.py
+++ envVar.py
@@ -1,13 +1,14 @@
 firebaseConfig = {
-    'apiKey': "AIzaSyDTMWv-92WG5xZ1NvcVb7WlE0mjPAiosY4",
-    'authDomain': "ivslive.firebaseapp.com",
-    'projectId': "ivslive",
-    'storageBucket': "ivslive.appspot.com",
-    'messagingSenderId': "846488367861",
-    'appId': "1:846488367861:web:581efb46cf0210326a4bbc",
-	'databaseURL' : "https://ivslive-default-rtdb.firebaseio.com/",
-    'measurementId': "G-RBWZWSHQSQ"
+	    'apiKey': "AIzaSyDR-a5PGjXpXFjvJVS9Ep3FOKXnNy9BsZg",
+	  'authDomain': "fundmang-42ad8.firebaseapp.com",
+	  'databaseURL': "https://fundmang-42ad8-default-rtdb.firebaseio.com",
+	  'projectId': "fundmang-42ad8",
+	  'storageBucket': "fundmang-42ad8.appspot.com",
+	  'messagingSenderId': "361815074904",
+	  'appId': "1:361815074904:web:8504dfd52dbde0b186422d",
+	  'measurementId': "G-MVMXL8CJNK"
   }
+
 import mysql.connector
 
 myDataBase = mysql.connector.connect(host="localhost", user="root", passwd="12345",
